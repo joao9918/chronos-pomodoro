2b72f3914fefc5cbc45f8a348b162a577de3a2d7
Joao Gabriel <joaogabriel9918@gmail.com>
2025-12-05 10:35:37 -0300

    Organizando Projeto em Pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/global.css";
-import { TimerIcon } from "lucide-react";
+import { Container } from "./components/Container";
+import { Heading } from "./components/Heading";
 
 export function App() {
   return (
     <>
-      <Heading>
-        Ola, pessoal!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui facere
-        cumque similique inventore consectetur eum vero temporibus, magnam,
-        reprehenderit debitis pariatur excepturi doloribus! Quos possimus ab
-        consequuntur eveniet minima tempora!
-      </p>
+      <Container>
+        <Heading>Menu</Heading>
+      </Container>
+
+      <Container>
+        <Heading>Logo</Heading>
+      </Container>
     </>
   );
 }
