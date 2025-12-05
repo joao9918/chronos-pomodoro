10e0525e26b8354f7b59dd01b07c7c8ab91e4044
Joao Gabriel <joaogabriel9918@gmail.com>
2025-12-05 19:35:15 -0300

    Criando primeiros componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+
 import { Container } from "./components/Container";
-import { Heading } from "./components/Heading";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 export function App() {
   return (
     <>
       <Container>
-        <Heading>Menu</Heading>
+        <Logo></Logo>
       </Container>
 
       <Container>
-        <Heading>Logo</Heading>
+        <Menu />
       </Container>
     </>
   );
