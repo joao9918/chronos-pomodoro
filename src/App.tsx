51789d1c767cfb4436d3e867cdf382dff940c5b9
Joao Gabriel <joaogabriel9918@gmail.com>
2025-12-08 18:00:14 -0300

    Finalizando Layout da Pagina Principal

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,10 @@ import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
 import { DefaultInput } from "./components/DefaultInput";
+import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
+import { Footer } from "./components/Footer";
+import { PlayCircleIcon } from "lucide-react";
 
 export function App() {
   return (
@@ -25,7 +29,12 @@ export function App() {
       <Container>
         <form className="form">
           <div className="formRow">
-            <DefaultInput labelText="task:" id="meuInput" type="text" />
+            <DefaultInput
+              labelText="task:"
+              id="meuInput"
+              type="text"
+              placeholder="Digite algo"
+            />
           </div>
 
           <div className="formRow">
@@ -33,14 +42,17 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <p>Ciclos</p>
-            <p>0 0 0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className="formRow">
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} color="green" />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
