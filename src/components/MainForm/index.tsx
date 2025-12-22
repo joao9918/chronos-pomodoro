dabb2ee948d18476bbae285bd11e618c535dbe40
João <joaogabriel9918@gmail.com>
2025-12-22 11:14:12 -0300

    Iniciando projeto de outro pc

---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,79 @@
+import { PlayCircleIcon } from "lucide-react";
+import { Cycles } from "../Cycles";
+import { DefaultButton } from "../DefaultButton";
+import { DefaultInput } from "../DefaultInput";
+import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
+
+export function MainForm() {
+  const { state, setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  //ciclos
+  const nextCycle = getNextCycle(state.currentCycle);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (taskNameInput.current === null) return;
+
+    const TaskName = taskNameInput.current.value.trim();
+
+    if (!TaskName) {
+      alert("Digite o nome da tarefa");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: TaskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: "workTime",
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: "00:00",
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
+  return (
+    <form onSubmit={handleCreateNewTask} className="form" action="">
+      <div className="formRow">
+        <DefaultInput
+          labelText="task"
+          id="meuInput"
+          type="text"
+          placeholder="Digite algo"
+          ref={taskNameInput}
+        />
+      </div>
+
+      <div className="formRow">
+        <p>Próximo intervalo é de 25min</p>
+      </div>
+
+      <div className="formRow">
+        <Cycles />
+      </div>
+
+      <div className="formRow">
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+}
