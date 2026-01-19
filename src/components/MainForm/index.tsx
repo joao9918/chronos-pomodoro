69c12a71181bc51c92d501f7ca76469ef6501c31
João <joaogabriel9918@gmail.com>
2026-01-19 19:57:24 -0300

    feat: CycleDots are working now, when you start a task

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -63,6 +63,7 @@ export function MainForm() {
           type="text"
           placeholder="Digite algo"
           ref={taskNameInput}
+          disabled={!!state.activeTask}
         />
       </div>
 
@@ -70,9 +71,11 @@ export function MainForm() {
         <p>Próximo intervalo é de 25min</p>
       </div>
 
-      <div className="formRow">
-        <Cycles />
-      </div>
+      {state.currentCycle !== 0 && (
+        <div className="formRow">
+          <Cycles />
+        </div>
+      )}
 
       <div className="formRow">
         <DefaultButton icon={<PlayCircleIcon />} />
