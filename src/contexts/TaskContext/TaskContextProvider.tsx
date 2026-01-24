b36dc3e7e9694084e14e32110804a02213fc8021
João <joaogabriel9918@gmail.com>
2026-01-24 16:10:18 -0300

    feat: Created the worker to control the time of the timer

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,7 @@ import { useEffect, useReducer } from "react";
 import { TaskContext } from "./TaskContext";
 import { initialTaskState } from "./initialTaskState";
 import { taskReducer } from "./taskReducer";
+import { TimerWorkerManager } from "../../worker/TimerWorkerManager";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -10,9 +11,21 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
 
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage((e) => {
+    const countDownSeconds = e.data;
+    console.log(countDownSeconds);
+  });
+
   useEffect(() => {
-    console.log(state);
-  }, [state]);
+    if (!state.activeTask) {
+      console.log("Worker terminado por falta de activeTask");
+      worker.terminate();
+    }
+
+    worker.postMessage(state);
+  }, [worker, state]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
