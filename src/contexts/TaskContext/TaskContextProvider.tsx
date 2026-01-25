6d1769d616bfb9187745b92c05ed7452547244f1
João <joaogabriel9918@gmail.com>
2026-01-24 21:23:40 -0300

    feat: Sincronizing the timer with the seconds remaingns

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -3,6 +3,7 @@ import { TaskContext } from "./TaskContext";
 import { initialTaskState } from "./initialTaskState";
 import { taskReducer } from "./taskReducer";
 import { TimerWorkerManager } from "../../worker/TimerWorkerManager";
+import { TaskActionTypes } from "./taskAction";
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -16,6 +17,16 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
   worker.onmessage((e) => {
     const countDownSeconds = e.data;
     console.log(countDownSeconds);
+
+    if (countDownSeconds <= 0) {
+      dispatch({ type: TaskActionTypes.COMPLETE_TASK });
+      worker.terminate();
+    } else {
+      dispatch({
+        type: TaskActionTypes.COUNT_DOWN,
+        payload: { secondsRemaining: countDownSeconds },
+      });
+    }
   });
 
   useEffect(() => {
--- src/contexts/TaskContext/taskAction.ts
+++ src/contexts/TaskContext/taskAction.ts
@@ -4,6 +4,8 @@ export const TaskActionTypes = {
   START_TASK: "START_TASK",
   INTERRUPT_TASK: "INTERRUPT_TASK",
   RESET_STATE: "RESET_STATE",
+  COUNT_DOWN: "COUNT_DOWN",
+  COMPLETE_TASK: "COMPLETE_TASK",
 } as const;
 
 export type TaskActionTypeValues =
@@ -12,4 +14,9 @@ export type TaskActionTypeValues =
 export type TaskActionModel =
   | { type: typeof TaskActionTypes.START_TASK; payload: TaskModel }
   | { type: typeof TaskActionTypes.INTERRUPT_TASK }
-  | { type: typeof TaskActionTypes.RESET_STATE };
+  | { type: typeof TaskActionTypes.RESET_STATE }
+  | {
+      type: typeof TaskActionTypes.COUNT_DOWN;
+      payload: { secondsRemaining: number };
+    }
+  | { type: typeof TaskActionTypes.COMPLETE_TASK };
